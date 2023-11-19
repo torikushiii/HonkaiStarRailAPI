425aba9d9a7917982965745d9cb8b21ea4c781f4
torikushiii <torikushiii@gmail.com>
2023-11-19 17:46:17 +0800

    moved `Prydwen` to defaults

--- resolvers/defaults/index.js
+++ resolvers/defaults/index.js
@@ -1,12 +1,14 @@
 const EurogamerResolver = require("./eurogamer-resolver.js");
 const Game8Resolver = require("./game8-resolver.js");
 const PolygonResolver = require("./polygon-resolver.js");
+const PrydwenResolver = require("./prydwen-resolver.js");
 
 const fetch = async () => {
 	const promises = await Promise.all([
 		EurogamerResolver.fetch(),
 		Game8Resolver.fetch(),
-		PolygonResolver.fetch()
+		PolygonResolver.fetch(),
+		PrydwenResolver.fetch()
 	]);
 
 	return [...new Set(promises.flat())];
