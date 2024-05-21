499e306f3dabd41e0dc9073f87046e22af106e86
torikushiii <torikushiii@gmail.com>
2024-05-22 04:36:18 +0800

    add language support for codes
    - Adds a new query parameter `lang` to the /code endpoint
    - Allows the user to fetch codes with localized material names
    - Uses the `localizedMaterials` function from app.Utils to fetch localized materials
    - Creates two arrays, `active` and `inactive` containing only active or inactive codes respectively

--- routes/code.js
+++ routes/code.js
@@ -2,11 +2,32 @@ module.exports = function (fastify, opts, done) {
 	const Router = fastify;
 
 	Router.get("/", async (req, res) => {
+		const lang = req.query.lang || "en";
+
 		const codes = await app.Query.collection("codes").find({}).toArray();
 		if (codes.length === 0) {
 			return res.send({
 				active: [],
 				inactive: []
+			});
+		}
+
+		if (lang !== "en") {
+			const rewards = await app.Utils.localizedMaterials(codes, lang);
+
+			const active = rewards.filter((code) => code.active).map(i => ({
+				code: i.code,
+				rewards: i.materials
+			}));
+
+			const inactive = rewards.filter((code) => !code.active).map(i => ({
+				code: i.code,
+				rewards: i.materials
+			}));
+
+			return res.send({
+				active,
+				inactive
 			});
 		}
 
